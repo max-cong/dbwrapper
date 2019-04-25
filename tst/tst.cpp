885742aff7c27166b172adca73ca2175628affea
Max <savagecm@users.noreply.github.com>
2019-04-25 09:39:33 +0800

    add gtest

--- tst/tst.cpp
+++ tst/tst.cpp
@@ -2,9 +2,11 @@
 #include <string>
 #include <thread>
 #include <chrono>
+#include <memory>
 #include "redisAsyncClient.hpp"
 #include "hiredis/hiredis.h"
 
+#include <gtest/gtest.h>
 void getCallback(redisAsyncContext *c, void *r, void *privdata)
 {
     redisReply *reply = (redisReply *)r;
@@ -12,25 +14,43 @@ void getCallback(redisAsyncContext *c, void *r, void *privdata)
     {
         if (c->errstr)
         {
-            __LOG(debug, "errstr: %s"<< c->errstr);
+            __LOG(debug, "errstr: %s" << c->errstr);
         }
         return;
     }
-     __LOG(debug, "private data is : "<<(void*)privdata<<", string is : "<<reply->str);
-   
+    __LOG(debug, "private data is : " << (void *)privdata << ", string is : " << reply->str);
 }
-int main()
+class redisAsyncClientTest : public testing::Test
 {
-    set_logLevel(loggerIface::logLevel::debug);
-    redisAsyncClient aclient;
+protected:
+    virtual void SetUp() override
+    {
+        set_logLevel(loggerIface::logLevel::debug);
+        _aclient_sptr = std::make_shared<redisAsyncClient>();
 
-    configCenter::cfgPropMap _config;
-    _config[PROP_HOST] = "127.0.0.1";
-    configCenter::configCenter<void *>::instance()->set_properties(aclient.getThis(), _config);
-    
-    aclient.init();
+        configCenter::cfgPropMap _config;
+        _config[PROP_HOST] = "127.0.0.1";
+        configCenter::configCenter<void *>::instance()->set_properties(_aclient_sptr->getThis(), _config);
 
-    std::this_thread::sleep_for(std::chrono::milliseconds(500));
-    aclient.put("hello", "world", NULL, getCallback);
-    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        _aclient_sptr->init();
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+    virtual void TearDown() override
+    {
+        // ...
+    }
+    std::shared_ptr<redisAsyncClient> _aclient_sptr;
+};
+
+TEST_F(redisAsyncClientTest, put)
+{
+    _aclient_sptr->put("hello", "world", NULL, getCallback);
+    EXPECT_EQ(0, 0);
 }
+
+int main(int argc, char *argv[])
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
